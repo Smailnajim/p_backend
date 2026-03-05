5702b62b3eb0eceaeb699607d9d571ebb8fb0e47
smailnajim <smailnajim2003@gmail.com>
2026-03-05 16:37:06 +0000

    a

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization')?.replace('Bearer ', '');
 
@@ -9,7 +10,18 @@ const auth = (req, res, next) => {
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(decoded.user.id);
+        if (!user) {
+            return res.status(401).json({ success: false, message: 'User not found' });
+        }
+
+        if (user.status === 'blocked') {
+            return res.status(403).json({ success: false, message: 'Your account is blocked. You cannot continue.' });
+        }
+
         req.user = decoded;
+        console.log('decoded **', decoded);
         next();
     } catch (error) {
         res.status(401).json({ success: false, message: 'Token is not valid' });
